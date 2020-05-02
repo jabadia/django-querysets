7573f905bbc820e39ddd9634239067be616ee78e
Javier Abadía <javi.abadia@gmail.com>
2020-05-02 19:41:03 +0200

    logging db queries

--- querysets/settings.py
+++ querysets/settings.py
@@ -15,7 +15,6 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
@@ -26,7 +25,6 @@ SECRET_KEY = 'su87)lhudg*9p$nn(r^fz9)*!y=1$28&fewhio!lk&cqix-qfh'
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -69,7 +67,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'querysets.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
@@ -79,7 +76,6 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
@@ -99,7 +95,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
 
@@ -113,8 +108,43 @@ USE_L10N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'simple': {
+            'format': '%(levelname)-8s %(message)s [%(pathname)s:%(lineno)d]',
+        },
+        'extended': {
+            'format': '%(levelname)-8s [%(asctime)s] %(message)s [%(pathname)s:%(lineno)d]',
+            'datefmt': '%Y-%m-%d %H:%M:%S',
+        },
+    },
+    'handlers': {
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+            'formatter': 'extended',
+        },
+    },
+    'loggers': {
+        'django': {
+            'level': 'INFO',
+        },
+        'requests': {
+            'level': 'WARNING',
+        },
+        '': {
+            'handlers': ['console'],
+            'level': 'DEBUG',
+        },
+        'django.db': {
+            'level': 'DEBUG',  # log SQL queries
+        },
+    }
+}
