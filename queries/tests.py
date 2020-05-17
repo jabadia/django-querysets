d5b6fb59163fc1b83dd3211a85bd99b3d4e2eb40
Javier Abadía <javi.abadia@gmail.com>
2020-05-17 17:13:45 +0200

    comment log queries

--- queries/tests.py
+++ queries/tests.py
@@ -9,7 +9,7 @@ class TestViews(TestCase):
     def setUp(self) -> None:
         super().setUp()
         # need to run tests with --debug-mode for this setting to be effective
-        logging.getLogger('django.db').setLevel(logging.DEBUG)
+        # logging.getLogger('django.db').setLevel(logging.DEBUG)
 
     def tearDown(self) -> None:
         super().tearDown()
