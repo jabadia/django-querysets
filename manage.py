2c8494c4298c98e00715d6acbc7d98b3d57a65ee
Javier Abadía <javi.abadia@gmail.com>
2020-05-17 17:33:30 +0200

    explicit settings mandatory

--- manage.py
+++ manage.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'querysets.settings')
+    assert 'DJANGO_SETTINGS_MODULE' in os.environ, "please define DJANGO_SETTINGS_MODULE explicitly"
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
